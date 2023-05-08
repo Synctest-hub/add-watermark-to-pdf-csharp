fb0944b6c2983df3a403b20fddcda96578eeb8a3
sameerkhan001 <sameerkhan.n@syncfusion.com>
2023-05-08 15:19:25 +0530

    828251 Added sample project and Readme file in watermark blog.

---
+++ Image_Watermark_PDF/Image_Watermark_PDF/Program.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Parsing;
+using Syncfusion.Pdf;
+using Syncfusion.Drawing;
+using Syncfusion.Pdf.Interactive;
+
+FileStream docStream = new FileStream("../../../Data/Input.pdf", FileMode.Open, FileAccess.Read);
+//Load the PDF document
+PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
+PdfLoadedPage lpage = loadedDocument.Pages[0] as PdfLoadedPage;
+//Creates PDF watermark annotation 
+PdfWatermarkAnnotation watermark = new PdfWatermarkAnnotation(new RectangleF(100, 300, 400, 400));
+//Sets properties to the annotation 
+watermark.Opacity = 0.5f;
+//Create the appearance of watermark 
+FileStream imageStream = new FileStream("../../../Data/Image.jpg", FileMode.Open, FileAccess.Read);
+PdfImage image = new PdfBitmap(imageStream);
+watermark.Appearance.Normal.Graphics.DrawImage(image, new PointF(0, 0));
+//Adds the annotation to page 
+lpage.Annotations.Add(watermark);
+
+//Create file stream.
+using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+{
+    //Save the PDF document to file stream.
+    loadedDocument.Save(outputFileStream);
+}
+//Close the document.
+loadedDocument.Close(true);
+
---
+++ Text_Watermark_PDF/Text_Watermark_PDF/Program.cs
@@ -0,0 +1,27 @@
+﻿using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf.Parsing;
+using Syncfusion.Pdf;
+using Syncfusion.Drawing;
+
+//Load the PDF document
+FileStream inputStream = new FileStream("../../../Input.pdf", FileMode.Open, FileAccess.Read);
+PdfLoadedDocument loadedDocument = new PdfLoadedDocument(inputStream);
+//Get the page  
+PdfLoadedPage lpage = loadedDocument.Pages[0] as PdfLoadedPage;
+
+//Creates PDF watermark annotation  
+PdfWatermarkAnnotation watermark = new PdfWatermarkAnnotation(new RectangleF(100, 300, 400, 400));
+//Sets properties to the annotation  
+watermark.Opacity = 0.5f;
+//Create the appearance of watermark  
+watermark.Appearance.Normal.Graphics.DrawString("Imported using Essential PDF", new PdfStandardFont(PdfFontFamily.Helvetica, 20), PdfBrushes.Red, new RectangleF(50, 50, 250, 50), new PdfStringFormat(PdfTextAlignment.Center, PdfVerticalAlignment.Middle));
+//Adds the annotation to page  
+lpage.Annotations.Add(watermark);
+
+FileStream stream = new FileStream("../../../Output.pdf", FileMode.Create);
+//Save the modified document to file. 
+loadedDocument.Save(stream);
+//Close the PDF document. 
+loadedDocument.Close(true);
+stream.Close();
